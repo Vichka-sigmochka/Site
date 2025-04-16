9ddb471a0d68938fbc9d5794aa9b949273c6722d
Оля <Olga1809dok@rambler.ru>
2025-04-16 20:15:12 +0300

    черновик формы регистрации

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/<word>')
+@app.route('/index/<word>')
+def index(word):
+    return render_template('base.html', title=word)
 
 
-@app.route('/')
+#@app.route('/training/<prof>')
+#def training(prof):
+    #return render_template('training.html', prof=prof)
 
-def title():
-    return "Миссия Колонизация Марса"
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list_prof.html', param=list)
 
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
