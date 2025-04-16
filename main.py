9624e1741c8748a36a0a1c8728bd45ced3fa72dd
Оля <Olga1809dok@rambler.ru>
2025-04-16 19:07:13 +0300

    первая программа

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def title():
+    return "Миссия Колонизация Марса"
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
